f295e57740690a6e047e3551ffb269bb94722d95
Jan Łakomy <janpiotrlakomy@gmail.com>
2026-09-05 11:51:03 +0200

    Refactor experiments into a single jupyter notebook

--- Anderson.jl
+++ Anderson.jl
@@ -17,8 +17,8 @@ function anderson(x0::AbstractVector{T}, g, m::Integer; β=1.0::AbstractFloat, t
     X = Matrix{T}(undef, length(x0), 0)
     F = Matrix{T}(undef, length(x0), 0)
 
-    history = [copy(x0)]
-    residual_history = [norm(f(x0))]
+    history = [copy(x0), copy(x_curr)]
+    residual_history = [norm(f(x0)), norm(f_curr)]
 
     for j in 1:max_iter
         if j > m 
@@ -35,8 +35,7 @@ function anderson(x0::AbstractVector{T}, g, m::Integer; β=1.0::AbstractFloat, t
         # solve f_j = F_j * γ for γ
         γ = F \ f_curr
 
-        push!(residual_history, norm(f_curr))
-        push!(history, x_curr)
+
 
         x_prev = x_curr
         f_prev = f_curr
@@ -44,6 +43,8 @@ function anderson(x0::AbstractVector{T}, g, m::Integer; β=1.0::AbstractFloat, t
         x_curr = (x_prev - X * γ) + β * (f_prev - F * γ)
         f_curr = f(x_curr)
 
+        push!(residual_history, norm(f_curr))
+        push!(history, x_curr)
         if (norm(f_curr) < tol)
             return (x = x_curr,stats=( residual = norm(f_curr), iterations = j,  history = history, residual_history = residual_history))
         end
